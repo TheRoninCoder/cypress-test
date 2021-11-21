4c65f66c86d99785150bbfce012fa02e4735b606
IK <iqbalkhan2020@protonmail.com>
2021-11-21 21:34:05 +0000

    Revert index.js

--- project-1/cypress/plugins/index.js
+++ project-1/cypress/plugins/index.js
@@ -1,4 +1,4 @@
-<reference types="cypress" />
+/// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -15,10 +15,8 @@
 /**
  * @type {Cypress.PluginConfig}
  */
- const {commitInfo} = require('@cypress/commit-info')
- // default folder is current working directory
- module.exports = on => {
-  on('file:preprocessor', file => {
-    commitInfo().then("branch", "message");
-  });
-};
+// eslint-disable-next-line no-unused-vars
+module.exports = (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+}
